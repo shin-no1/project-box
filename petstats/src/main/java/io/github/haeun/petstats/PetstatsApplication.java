962e351acf77a93e226554799f629e5afbee19bc
shin-no1 <shinhe524@gmail.com>
2025-04-24 00:48:44 +0900

    add: module petstats

---
+++ petstats/src/main/java/io/github/haeun/petstats/PetstatsApplication.java
@@ -0,0 +1,13 @@
+package io.github.haeun.petstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetstatsApplication.class, args);
+    }
+
+}
---
+++ petstats/src/test/java/io/github/haeun/petstats/PetstatsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haeun.petstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetstatsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
