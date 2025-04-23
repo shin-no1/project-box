6f2523396be19d75e990e1d718ed5e6350a03347
shin-no1 <shinhe524@gmail.com>
2025-04-23 12:26:12 +0900

    create: carbon_scope

---
+++ carbon_scope/src/main/java/io/github/haeun/carbon_scope/CarbonScopeApplication.java
@@ -0,0 +1,13 @@
+package io.github.haeun.carbon_scope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonScopeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarbonScopeApplication.class, args);
+    }
+
+}
---
+++ carbon_scope/src/test/java/io/github/haeun/carbon_scope/CarbonScopeApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.haeun.carbon_scope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonScopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
