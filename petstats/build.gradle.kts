823b5c984385f4ffdcc3f63e3282f20a85a1259b
shin-no1 <shinhe524@gmail.com>
2025-04-24 15:19:15 +0900

    build: [petstats] Querydsl 의존성 추가

--- petstats/build.gradle.kts
+++ petstats/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.springBoot
+
 plugins {
     java
     id("org.springframework.boot") version "3.4.4"
@@ -6,6 +8,7 @@ plugins {
 
 group = "io.github.haeun"
 version = "0.0.1-SNAPSHOT"
+val queryDslVersion = "5.0.0" // QueryDSL Version Setting
 
 java {
     toolchain {
@@ -31,8 +34,36 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    // QueryDSL Implementation
+    implementation ("com.querydsl:querydsl-jpa:${queryDslVersion}:jakarta")
+    annotationProcessor("com.querydsl:querydsl-apt:${queryDslVersion}:jakarta")
+    annotationProcessor("jakarta.annotation:jakarta.annotation-api")
+    annotationProcessor("jakarta.persistence:jakarta.persistence-api")
 }
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+/**
+ * QueryDSL Build Options
+ */
+val querydslDir = "src/main/generated"
+
+sourceSets {
+    getByName("main").java.srcDirs(querydslDir)
+}
+
+tasks.withType<JavaCompile> {
+    options.generatedSourceOutputDirectory = file(querydslDir)
+
+    // 위의 설정이 안되면 아래 설정 사용
+    // options.generatedSourceOutputDirectory.set(file(querydslDir))
+}
+
+tasks.named("clean") {
+    doLast {
+        file(querydslDir).deleteRecursively()
+    }
 }
