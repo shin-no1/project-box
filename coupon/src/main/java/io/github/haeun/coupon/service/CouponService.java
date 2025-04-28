677898e5b9f80dbc7bcbac621840785e80155fc5
shin-no1 <shinhe524@gmail.com>
2025-04-28 16:24:24 +0900

    [coupon] fix: remove coupon init

--- coupon/src/main/java/io/github/haeun/coupon/service/CouponService.java
+++ coupon/src/main/java/io/github/haeun/coupon/service/CouponService.java
@@ -14,34 +14,6 @@ public class CouponService {
     private static final String COUPON_STOCK_PREFIX = "coupon:";
 
     /**
-     * 쿠폰 재고를 Redis에 초기화하는 메서드
-     *
-     * @param request
-     * - couponId 쿠폰 ID
-     * - quantity 초기 재고 수량
-     * - ttlSeconds TTL (초 단위, 선택값. null이면 무제한 저장)
-     */
-    public void initCouponStock(CouponStockRequest request) {
-        if (request.getCouponId() == null || request.getQuantity() <= 0) {
-            // TODO: 에러 예외처리
-            throw new IllegalArgumentException("couponId와 quantity는 필수이며, quantity는 1 이상이어야 합니다.");
-        }
-
-        String key = COUPON_STOCK_PREFIX + request.getCouponId() + ":stock";
-
-        try {
-            if (request.getTtlSeconds() > 0) {
-                redisTemplate.opsForValue().set(key, request.getQuantity(), Duration.ofSeconds(request.getTtlSeconds()));
-            } else {
-                redisTemplate.opsForValue().set(key, request.getQuantity());
-            }
-        } catch (Exception e) {
-            // TODO: 에러 예외처리
-            throw new RuntimeException("쿠폰 초기화 중 Redis 오류 발생", e);
-        }
-    }
-
-    /**
      * 쿠폰 재고 수량을 Redis에서 조회하는 메서드
      *
      * @param couponId 쿠폰 ID
--- coupon/src/main/java/io/github/haeun/coupon/web/CouponController.java
+++ coupon/src/main/java/io/github/haeun/coupon/web/CouponController.java
@@ -13,16 +13,6 @@ import org.springframework.web.bind.annotation.*;
 public class CouponController {
     private final CouponService couponService;
 
-    @PostMapping("/init")
-    public ResponseEntity<String> initCouponStock(CouponStockRequest request) {
-        try {
-            couponService.initCouponStock(request);
-            return ResponseEntity.ok("쿠폰 초기화 완료");
-        } catch (Exception e) {
-            return ResponseEntity.badRequest().body("에러 발생");
-        }
-    }
-
     @GetMapping("/{couponId}")
     public ResponseEntity<?> getCouponStock(@PathVariable Long couponId) {
         Integer stock = couponService.getCouponStock(couponId);
