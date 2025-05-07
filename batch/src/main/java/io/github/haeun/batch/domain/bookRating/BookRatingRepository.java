29a175b56a7e5c268aec47266dca1633ba674ebc
shin-no1 <shinhe524@gmail.com>
2025-05-07 22:45:17 +0900

    [batch] remove: write test

--- batch/src/main/java/io/github/haeun/batch/domain/bookRating/BookRatingRepository.java
+++
@@ -1,6 +0,0 @@
-package io.github.haeun.batch.domain.bookRating;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BookRatingRepository extends JpaRepository<BookRating, Long> {
-}
